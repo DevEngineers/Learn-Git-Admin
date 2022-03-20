16e3207bedfa460b7dffdb962e6d09869b70d60d
Zumry <amzumry@gmail.com>
2022-03-20 22:55:21 +0530

    Created Custom Text Field

---
+++ lib/components/custom_textField..dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFiled extends StatelessWidget {
+  final String label;
+  final Function onChange;
+
+  const CustomTextFiled({Key? key, required this.label, required this.onChange})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+        decoration: InputDecoration(
+          border: const OutlineInputBorder(),
+          helperText: label,
+          labelText: label,
+        ),
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return "Please Enter " + label;
+          }
+          return null;
+        },
+        onSaved: (String? value) {
+          onChange(value);
+        });
+  }
+}
