487e6a234d0bfe36cb2fe053984b457c55a33649
Zumry <amzumry@gmail.com>
2022-03-22 07:15:04 +0530

    error fixed

--- lib/components/custom_text_field.dart
+++ lib/components/custom_text_field.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 
 class CustomTextFiled extends StatelessWidget {
   final String label;
+  final int? maxLine, minLine;
   final Function onChange;
 
   const CustomTextFiled({
     Key? key,
     required this.label,
     required this.onChange,
+    this.maxLine,
+    this.minLine,
   }) : super(key: key);
 
   @override
@@ -15,9 +18,11 @@ class CustomTextFiled extends StatelessWidget {
     return TextFormField(
         decoration: InputDecoration(
           border: const OutlineInputBorder(),
-          helperText: label,
+          // helperText: label,
           labelText: label,
         ),
+        minLines: minLine,
+        maxLines: maxLine,
         validator: (value) {
           if (value == null || value.isEmpty) {
             return "Please Enter " + label;
