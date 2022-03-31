256a9559c7411e0bb97bc26f79a55ff3a951b77a
Zumry <amzumry@gmail.com>
2022-03-31 11:34:09 +0530

    add question service implemented

--- lib/services/QuestionService.dart
+++ lib/services/QuestionService.dart
@@ -1,14 +1,24 @@
 import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart';
-
 import '../model/question.dart';
 
 class QuestionService {
-  static const String endpoint = "";
+  static String endpoint = '${dotenv.env['API_URL']}/question';
   const QuestionService();
 
-  Future<Response> addQuestion() async {
-    return null;
+  Future<bool?> addQuestion(Set question) async {
+    print('question Service Q : - ${question}');
+    final response = await post(Uri.parse(endpoint),
+        headers: {
+          'Content-type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: json.encode(question));
+    if (response.statusCode == 200) {
+      return true;
+    }
+    throw Exception("Error in Getting the Question post");
   }
 
   Future<List<Question>?> getQuestions() async {
