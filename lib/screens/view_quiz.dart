08a1053f968e34d4f550299191cd6cb70eabdad1
Zumry <amzumry@gmail.com>
2022-04-05 03:02:07 +0530

    data binding

--- lib/screens/view_quiz.dart
+++ lib/screens/view_quiz.dart
@@ -1,53 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:learn_git_admin/provider/question_provider.dart';
+import 'package:provider/provider.dart';
 import '../components/custom_text.dart';
 import '../model/question.dart';
 
-const List<Question> q = [
-  Question(
-      id: '1',
-      topicId: '2',
-      question: 'What is the git branching?',
-      answers: ['1', '2'],
-      correctAnswer: '2'),
-  Question(
-      id: '2',
-      topicId: '2',
-      question: 'What is the git branching?2',
-      answers: ['1', '2'],
-      correctAnswer: '1')
-];
+class ViewQuestion extends StatefulWidget {
+  static const String routeName = '/view_question';
+  const ViewQuestion({Key? key}) : super(key: key);
 
-class AnswersList extends StatelessWidget {
-  final String answer;
-  final String groupValue;
-  const AnswersList({Key? key, required this.answer, required this.groupValue})
-      : super(key: key);
+  @override
+  State<StatefulWidget> createState() => _ViewQuestion();
+}
+
+class _ViewQuestion extends State<ViewQuestion> {
+  String topicId = '6238c42e523b9f9d1325096e';
+
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.maxFinite,
-      height: 70,
-      child: Card(
-        elevation: 3,
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: RichText(
-                  text: TextSpan(
-                      text: '• ',
-                      style: const TextStyle(
-                          color: Colors.black,
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold),
-                      children: <TextSpan>[TextSpan(text: '  $answer')]),
-                ),
-              ),
-            ]),
-      ),
-    );
+    final Set<Question> _questions =
+        Provider.of<QuestionProvider>(context).getQuestionsByTopic(topicId);
+
+    return Scaffold(
+        appBar: AppBar(title: const Text('Questions')),
+        body: SingleChildScrollView(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+                padding: EdgeInsets.all(8),
+                child: CustomText(
+                  text: 'Learn Basic Git',
+                  type: 'headText',
+                )),
+            ListView.builder(
+              physics: const ClampingScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: _questions.length,
+              itemBuilder: (context, index) {
+                return QuizQuestion(
+                  question: _questions.elementAt(index),
+                  questionNo: index,
+                );
+              },
+            ),
+          ],
+        )));
   }
 }
 
@@ -55,9 +53,11 @@ class QuizQuestion extends StatefulWidget {
   final Question question;
   final int questionNo;
 
-  const QuizQuestion(
-      {Key? key, required this.question, required this.questionNo})
-      : super(key: key);
+  const QuizQuestion({
+    Key? key,
+    required this.question,
+    required this.questionNo,
+  }) : super(key: key);
 
   @override
   State<StatefulWidget> createState() => _QuizQuestion();
@@ -109,37 +109,41 @@ class _QuizQuestion extends State<QuizQuestion> {
   }
 }
 
-class ViewQuestion extends StatelessWidget {
-  static const String routeName = '/viewQuestion';
-  const ViewQuestion({Key? key}) : super(key: key);
+class AnswersList extends StatelessWidget {
+  final String answer;
+  final String groupValue;
+
+  const AnswersList({
+    Key? key,
+    required this.answer,
+    required this.groupValue,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: const Text('Quiz')),
-        body: SingleChildScrollView(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Padding(
-                padding: EdgeInsets.all(8),
-                child: CustomText(
-                  text: 'Learn Basic Git',
-                  type: 'headText',
-                )),
-            ListView.builder(
-              physics: const ClampingScrollPhysics(),
-              shrinkWrap: true,
-              itemCount: q.length,
-              itemBuilder: (context, index) {
-                return QuizQuestion(
-                  question: q.elementAt(index),
-                  questionNo: index,
-                );
-              },
-            ),
-          ],
-        )));
+    return SizedBox(
+      width: double.maxFinite,
+      height: 70,
+      child: Card(
+        elevation: 3,
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: RichText(
+                  text: TextSpan(
+                      text: '• ',
+                      style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold),
+                      children: <TextSpan>[TextSpan(text: '  $answer')]),
+                ),
+              ),
+            ]),
+      ),
+    );
   }
 }
