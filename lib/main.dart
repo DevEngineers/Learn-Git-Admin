6f6f3dec0cf9ee26c1c8a275b58732f4663f3c62
Zumry <amzumry@gmail.com>
2022-04-06 23:38:51 +0530

    file renamed

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,8 @@ import 'package:learn_git_admin/screens/question_home.dart';
 import 'package:learn_git_admin/screens/splash_screen.dart';
 import 'package:provider/provider.dart';
 import 'screens/home.dart';
-import 'screens/add_question.dart';
-import 'screens/view_question.dart';
+import 'screens/question_add_edit.dart';
+import 'screens/question_view.dart';
 
 void main() async {
   await dotenv.load();
