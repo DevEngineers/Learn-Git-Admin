508ea5fe07d6446cd5d3daedf7b65eddac070265
Zumry <amzumry@gmail.com>
2022-04-06 23:20:24 +0530

    question Count added in UI

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:learn_git_admin/providers/question_provider.dart';
 import 'package:learn_git_admin/screens/content_view.dart';
 import 'package:learn_git_admin/screens/question_home.dart';
 import 'package:provider/provider.dart';
@@ -17,6 +18,7 @@ class _Home extends State<Home> {
   @override
   Widget build(BuildContext context) {
     int tutorialCount = Provider.of<ContentProvider>(context).contents.length;
+    int questionCount = Provider.of<QuestionProvider>(context).questions.length;
 
     return Scaffold(
         appBar: AppBar(
@@ -60,7 +62,7 @@ class _Home extends State<Home> {
                   }),
               MenuItem(
                   title: 'Tutorial Questions',
-                  body: '5 Questions',
+                  body: '${questionCount.toString()} Questions',
                   onPress: () {
                     Navigator.of(context).pushNamed(QuestionHome.routeName);
                   }),
